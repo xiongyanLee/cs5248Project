985d5ff8b52f5a26fdb6bb21e0cadafe0a31e780
xiongyanLee <lxyfxwz@gmail.com>
2017-10-21 14:44:28 +0800

    add some ui

--- Test3/app/src/main/java/com/example/nuonuomisu/test3/CameraActivity.java
+++ Test3/app/src/main/java/com/example/nuonuomisu/test3/CameraActivity.java
@@ -2,6 +2,9 @@ package com.example.nuonuomisu.test3;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class CameraActivity extends Activity {
 
@@ -9,10 +12,28 @@ public class CameraActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        if (null == savedInstanceState) {
-            getFragmentManager().beginTransaction()
-                    .replace(R.id.container, Camera2VideoFragment.newInstance())
-                    .commit();
-        }
+        Button start_recording = findViewById(R.id.button_start);
+        start_recording.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //if (null == savedInstanceState) {
+                    getFragmentManager().beginTransaction()
+                            .replace(R.id.container, Camera2VideoFragment.newInstance())
+                            .commit();
+              //  }
+            }
+        });
+        Button stream = findViewById(R.id.stream_page);
+        stream.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                setContentView(R.layout.live_stream);
+            }
+        });
+        //Button record = findViewById(R.id.back_record);
+        //record.setOnClickListener(new View.OnClickListener() {
+         //   public void onClick(View v) {
+                //setContentView(R.layout.activity_camera);
+                //Toast.makeText(activity, "switch back", Toast.LENGTH_SHORT).show();
+          //  }
+       // });
     }
 }
