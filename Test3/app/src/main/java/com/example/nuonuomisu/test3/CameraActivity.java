375b9d8420db40c89647b252231f9eef4cc4d45f
xiongyanLee <lxyfxwz@gmail.com>
2017-10-29 17:52:52 +0800

    added player function

--- Test3/app/src/main/java/com/example/nuonuomisu/test3/CameraActivity.java
+++ Test3/app/src/main/java/com/example/nuonuomisu/test3/CameraActivity.java
@@ -1,33 +1,31 @@
 package com.example.nuonuomisu.test3;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ViewFlipper;
+
 
 public class CameraActivity extends Activity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        final ViewFlipper flipview = (ViewFlipper) findViewById(R.id.myViewFlipper);
         setContentView(R.layout.activity_camera);
+        //flipview.setDisplayedChild(flipview.indexOfChild(findViewById(R.id.record_page)));
         Button start_recording = findViewById(R.id.button_start);
         start_recording.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
             getFragmentManager().beginTransaction()
                     .replace(R.id.container, Camera2VideoFragment.newInstance())
                     .commit();
-
             }
         });
         Button stream = findViewById(R.id.stream_page);
-        stream.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                setContentView(R.layout.live_stream);
-            }
-        });
-
-
         //Button record = findViewById(R.id.back_record);
         //record.setOnClickListener(new View.OnClickListener() {
          //   public void onClick(View v) {
@@ -35,5 +33,15 @@ public class CameraActivity extends Activity {
                 //Toast.makeText(activity, "switch back", Toast.LENGTH_SHORT).show();
           //  }
        // });
+        //playerView = (SimpleExoPlayerView) findViewById(R.id.video_view);
     }
+
+    public void playerActivity(View view){
+        Intent intent  = new Intent(this, Player.class);
+        startActivity(intent);
+    }
+
+
+
+
 }
---
+++ Test3/app/src/main/java/com/example/nuonuomisu/test3/Player.java
@@ -0,0 +1,124 @@
+package com.example.nuonuomisu.test3;
+
+import android.annotation.SuppressLint;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.google.android.exoplayer2.DefaultLoadControl;
+import com.google.android.exoplayer2.DefaultRenderersFactory;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.dash.DashChunkSource;
+import com.google.android.exoplayer2.source.dash.DashMediaSource;
+import com.google.android.exoplayer2.source.dash.DefaultDashChunkSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.ui.SimpleExoPlayerView;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultHttpDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
+
+/**
+ * Created by xiongyan on 10/29/2017.
+ */
+
+public class Player extends AppCompatActivity {
+    private static final DefaultBandwidthMeter BANDWIDTH_METER = new DefaultBandwidthMeter();
+    private SimpleExoPlayer player;
+    private SimpleExoPlayerView playerView;
+
+    private long playbackPosition;
+    private int currentWindow;
+    private boolean playWhenReady = true;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.player);
+
+        playerView = (SimpleExoPlayerView) findViewById(R.id.video_view);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        if (Util.SDK_INT > 23) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        hideSystemUi();
+        if ((Util.SDK_INT <= 23 || player == null)) {
+            initializePlayer();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        if (Util.SDK_INT <= 23) {
+            releasePlayer();
+        }
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (Util.SDK_INT > 23) {
+            releasePlayer();
+        }
+    }
+
+    private void initializePlayer() {
+        if (player == null) {
+            // a factory to create an AdaptiveVideoTrackSelection
+            TrackSelection.Factory adaptiveTrackSelectionFactory =
+                    new AdaptiveTrackSelection.Factory(BANDWIDTH_METER);
+            player = ExoPlayerFactory.newSimpleInstance(new DefaultRenderersFactory(this),
+                    new DefaultTrackSelector(adaptiveTrackSelectionFactory), new DefaultLoadControl());
+            playerView.setPlayer(player);
+            player.setPlayWhenReady(playWhenReady);
+            player.seekTo(currentWindow, playbackPosition);
+        }
+        MediaSource mediaSource = buildMediaSource(Uri.parse(getString(R.string.media_url_dash)));
+        player.prepare(mediaSource, true, false);
+    }
+
+    private void releasePlayer() {
+        if (player != null) {
+            playbackPosition = player.getCurrentPosition();
+            currentWindow = player.getCurrentWindowIndex();
+            playWhenReady = player.getPlayWhenReady();
+            player.release();
+            player = null;
+        }
+    }
+
+    private MediaSource buildMediaSource(Uri uri) {
+        DataSource.Factory dataSourceFactory =
+                new DefaultHttpDataSourceFactory("ua", BANDWIDTH_METER);
+        DashChunkSource.Factory dashChunkSourceFactory =
+                new DefaultDashChunkSource.Factory(dataSourceFactory);
+
+        return new DashMediaSource(uri, dataSourceFactory,
+                dashChunkSourceFactory, null, null);
+    }
+
+    @SuppressLint("InlinedApi")
+    private void hideSystemUi() {
+        playerView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+}
